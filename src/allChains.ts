4adb7ebe1ea402c6ce1c34bc7d8fe6bcd26fb4d3
ligebit <31742363+ligebit@users.noreply.github.com>
2023-08-14 13:45:25 +0300

    upd explorerScanLike

--- src/allChains.ts
+++ src/allChains.ts
@@ -388,6 +388,6 @@ export const ALL_CHAINS: { [key in EChains]?: IChain } = {
         },
         isEvm: true,
         token: new TokenJSON(ALL_TOKENS[EChains.Rollux].find(el => el.coingeckoId == 'syscoin')),
-        explorerScanLike: 'https://rpc.rollux.com'
+        explorerScanLike: 'https://explorer.rollux.com/api'
     }
 }
